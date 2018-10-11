e95b242119f217e94dca717526d048f1a83695f1
= <jake.branch777@gmail.com>
2018-10-11 11:49:48 +0100

    added texture parameters

--- src/oluxEngine/Texture.cpp
+++ src/oluxEngine/Texture.cpp
@@ -38,6 +38,11 @@ namespace OluxEngine
 
 		glBindTexture(GL_TEXTURE_2D, rtn->id);
 
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
 		free(data);
 		glGenerateMipmap(GL_TEXTURE_2D);
